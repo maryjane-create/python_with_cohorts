93e4bb744dd483f815d72b18a465d62329951972
Mary Jane Okoroafor <maryjaneokoroafor77@gmail.com>
2022-01-26 19:29:48 +0100

    added changes

--- nine/maryjane/_array.py
+++ nine/maryjane/_array.py
@@ -3,6 +3,18 @@
 
 a_list=[]
 
-a_list.append(1)
-a_list.append(9)
-print( a_list)
+
+
+
+for elements in range(9):
+    newList=a_list.append(1)
+    if elements%2==0:
+        a_list[elements]=0
+    else:  pass
+
+
+
+print(a_list)
+# a_list.append(1)
+# a_list.append(9)
+# print( a_list)

e6e0c03c413ea46f9f23018c173dd3b875ff6815
Mary Jane Okoroafor <maryjaneokoroafor77@gmail.com>
2022-02-01 21:45:46 +0100

    rewrote

--- nine/maryjane/fourier_series.py
+++ nine/maryjane/fourier_series.py
@@ -11,6 +11,49 @@ for filename in myFiles:
 print(os.path.abspath('.//Scripts'))
 
 
+
+
+print(os.path.abspath('.'))
+
+print(os.path.isabs('.'))
+
+print(os.path.isabs(os.path.abspath('.')))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# print(os.path.isabs(os.path.abspath('.')))
 # print(os.path.abspath('.//Scripts'))
 
 
